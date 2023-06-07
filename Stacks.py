f5880368a411b80336ce9eff1bccd9b73238b6c1
aamrlyman <aamrlyman@gmail.com>
2023-06-06 20:37:58 -0700

    got the basic algorithm working. There are still lots of edge cases to check

--- Stacks.py
+++ Stacks.py
@@ -1,6 +1,9 @@
 # https://www.enjoyalgorithms.com/blog/application-of-stack-data-structure-in-programming
+# are expressions usually represented as strings?
+# What do you do about multi-digit numbers another for loop that adds all numbers to expression until an operator is reached?
+# spaces 
 
-exp = "(1 + 2) * 3"
+exp = "(1+2)*3"
 
 
 def Expression_Converter(expression):
@@ -9,7 +12,8 @@ def Expression_Converter(expression):
         "-": 1,
         "*": 2,
         "/": 2,
-        ")": 0
+        ")": 0,
+        "(": 0
     }
     stack = []
     postfix_expression = ""
@@ -19,17 +23,20 @@ def Expression_Converter(expression):
         elif x == "(":
             stack.append(x)
         elif x == ")":
-            for operator in stack:
+            for operator in range((len(stack) -1), 0, -1):
                 if operator == "(":
                     stack.pop()
                     break
                 else:
                     postfix_expression += stack.pop()
+        elif len(stack) == 0:
+            stack.append(x)
         else:
             for operator in stack:
-                if operators[operator] >= x:
+                if operators[operator] >= operators[x]:
                     postfix_expression += stack.pop()
                 stack.append(x)
+                break
     if len(stack) > 0:
         for operator in stack:
             postfix_expression += stack.pop()
@@ -39,3 +46,4 @@ x = Expression_Converter(exp)
 print(x)
 
 print(type("100"))
+
