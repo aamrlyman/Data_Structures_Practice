dc3be8066ccf51f011cab3208e53f1c386bf4889
aamrlyman <aamrlyman@gmail.com>
2024-02-06 18:37:58 -0800

    Finish time converter and unit tests

--- Algorithms/TimeConverter.py
+++ Algorithms/TimeConverter.py
@@ -3,8 +3,6 @@
 # and return the value converted to a new unit with the new unit and converting any remainder values into smaller units. 
 # integers and decimals will be handled
 
-# steps 
-# 
 from dataclasses import dataclass
 from enum import Enum
 
@@ -22,30 +20,37 @@ units: dict[UnitName,int] = {
     UnitName.millisecond: 1,
 }
 
+def formatOutPutString(outPutDict:dict[UnitName,int],negativeCase:str)->str:
+    outPutString = ""
+    outPutList:list[str] = []
+    for key,value in outPutDict.items():
+        if(value > 0):
+            plural_suffix = "s" if value > 1 else ""
+            unitString = f'{value} {key.value}{plural_suffix}'
+            outPutList.append(unitString)
+    outPutListLength = len(outPutList)
+    for index,string in enumerate(outPutList):
+        puncuation = "," if(outPutListLength > 1 and index < outPutListLength-1 ) else ""   
+        outPutString+=f'{negativeCase}{string}{puncuation} '
+    outPutString = outPutString.strip() if outPutString.strip() else "0" 
+    return outPutString
+
+
 def unit_conversion(amount:int, inputUnit:UnitName, outputUnit:UnitName, units: dict[UnitName,int])->str:
+    negativeCase = ""
+    if(amount < 0):
+        amount = abs(amount)
+        negativeCase = "-"
     unitOrder = [x for x in units]
     outPutDict = {}
-    outPutString = ""
     if(units[inputUnit] > units[outputUnit]): return f'{amount * units[inputUnit]//units[outputUnit]} {outputUnit.value}s'
     else: 
+        if(inputUnit.value != UnitName.millisecond):
+            amount *=units[inputUnit]
         for index in range(unitOrder.index(outputUnit), len(unitOrder)):
             outPutDict[unitOrder[index]] = amount // units[unitOrder[index]]
             remainder = amount % units[unitOrder[index]]
             amount = remainder
-    for key,value in outPutDict.items():
-        plural_suffix = "s" if value > 1 else ""
-        # puncuation = "" if value == UnitName.millisecond else ", " 
-        if(value > 0):
-            outPutString+= f'{value} {key.value}{plural_suffix}'
-    return outPutString
+    return formatOutPutString(outPutDict, negativeCase)
 
-amount = unit_conversion(3600000, UnitName.millisecond, UnitName.hour, units)
-print(amount)
-
-
-
-
-# put all units in relation to the smallest baseline unit. Sort it apply the logic 
-# name and relation to smallest unit 
-# 0 hours 0 minutes 45 seconds ?? 
 
--- tests/test_TimeConverter.py
+++ tests/test_TimeConverter.py
@@ -3,7 +3,17 @@ import pytest
 
 @pytest.mark.parametrize("amount, inputUnit, outputUnit, units, expected_output", [
     (3600000, UnitName.millisecond, UnitName.hour, units,"1 hour"),
+    (3800000, UnitName.millisecond, UnitName.hour, units,"1 hour, 3 minutes, 20 seconds"),
+    (3800856, UnitName.millisecond, UnitName.hour, units,"1 hour, 3 minutes, 20 seconds, 856 milliseconds"),
     (1, UnitName.hour, UnitName.millisecond, units, "3600000 milliseconds"),
+    (-125, UnitName.minute, UnitName.hour, units, "-2 hours, -5 minutes"),
+    (125, UnitName.minute, UnitName.hour, units, "2 hours, 5 minutes"),
+    (0, UnitName.minute, UnitName.hour, units, "0"),
+    (500, UnitName.minute, UnitName.hour, units, "8 hours, 20 minutes"),
+    (4600, UnitName.second, UnitName.hour, units, "1 hour, 16 minutes, 40 seconds"),
+    (4600, UnitName.second, UnitName.minute, units, "76 minutes, 40 seconds"),
+    (4600, UnitName.millisecond, UnitName.second, units, "4 seconds, 600 milliseconds"),
+    
    
 ])
 
